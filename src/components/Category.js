efad767893de6d5af8466182c77d778bd8be8dd1
Shan Joel <shan.joeld@gmail.com>
2022-05-18 03:01:56 +0530

    Added different cuisine types and created the filer functionality

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,27 +1,27 @@
 import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
 import { GiNoodles, GiChopsticks } from 'react-icons/gi';
 import styled from 'styled-components';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Category = () => {
    return (
       <List>
-         <div>
+         <SLink to={'/cuisine/Italian'}>
             <FaPizzaSlice />
             <h4>Italian</h4>
-         </div>
-         <div>
+         </SLink>
+         <SLink to={'/cuisine/American'}>
             <FaHamburger />
             <h4>American</h4>
-         </div>
-         <div>
+         </SLink>
+         <SLink to={'/cuisine/Thai'}>
             <GiNoodles />
             <h4>Thai</h4>
-         </div>
-         <div>
+         </SLink>
+         <SLink to={'/cuisine/Mexican'}>
             <GiChopsticks />
             <h4>Japanese</h4>
-         </div>
+         </SLink>
       </List>
    );
 };
@@ -32,16 +32,41 @@ const List = styled.div`
    align-items: center;
    margin: 2rem 0;
    gap: 1rem;
+`;
 
-   div {
-      min-width: 110px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      padding: 1rem;
-      background: #bdbdbd;
-      border-radius: 55%;
-      cursor: pointer;
+const SLink = styled(NavLink)`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   border-radius: 50%;
+   margin-right: 2rem;
+   text-decoration: none;
+   background: linear-gradient(35deg, #494949, #313131);
+   width: 6rem;
+   height: 6rem;
+   cursor: pointer;
+   transform: scale(0.8);
+
+   h4 {
+      color: #fff;
+      font-size: 0.8rem;
+   }
+
+   svg {
+      color: #fff;
+      font-size: 1.5rem;
+   }
+   &.active {
+      background: linear-gradient(to right, #f27121, #e94057);
+
+      svg {
+         color: #fff;
+      }
+
+      h4 {
+         color: #fff;
+      }
    }
 `;
 
